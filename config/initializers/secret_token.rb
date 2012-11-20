9c04d2c00358b6a71c9bfc1fc91f6dfe420a5b2f
Chaz Batrouney <chaz@chaz.com.au>
2012-11-20 19:13:16 +1100

    Started new rails environment

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Anzahpe13Com::Application.config.secret_token = '455d6cfbea7b082793f019d3668ad97b5ca2549f6b8cf3aba0f2923215d74825437c1a80d7060831b29a3cbb57e28f200f1c3470344a8b08ff29e37e44251a3c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Anzahpe13Com::Application.config.session_store :cookie_store, :key => '_anzahpe13.com_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Anzahpe13Com::Application.config.session_store :active_record_store
