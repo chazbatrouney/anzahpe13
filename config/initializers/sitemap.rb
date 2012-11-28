5317e64dcdc9cb71aed24255e363514c5f6d82f3
Chaz Batrouney <chaz@chaz.com.au>
2012-11-28 12:26:20 +1100

    Configured sitemap.xml for Google indexing

--- config/initializers/sitemap.rb
+++ config/initializers/sitemap.rb
@@ -3,26 +3,11 @@ DynamicSitemaps::Sitemap.draw do
   # default per_page is 50.000 which is the specified maximum.
   per_page 10
 
-  url root_url, :last_mod => DateTime.now, :change_freq => 'daily', :priority => 1
-  
-  new_page!
-  
-  Product.all.each do |product|
-    url product_url(product), :last_mod => product.updated_at, :change_freq => 'monthly', :priority => 0.8
-  end
-
-  new_page!
-  
-  autogenerate  :products, :categories,
-                :last_mod => :updated_at,
-                :change_freq => 'monthly',
-                :priority => 0.8
-                
-  new_page!
-  
-  autogenerate  :users,
-                :last_mod => :updated_at,
-                :change_freq => lambda { |user| user.very_active? ? 'weekly' : 'monthly' },
-                :priority => 0.5
-  
+  url main_index_url, :last_mod => DateTime.now, :change_freq => 'daily', :priority => 1
+  url main_program_url, :last_mod => DateTime.now, :change_freq => 'daily', :priority => 1
+  url main_sponsors_url, :last_mod => DateTime.now, :change_freq => 'daily', :priority => 1
+  url main_register_url, :last_mod => DateTime.now, :change_freq => 'daily', :priority => 1
+  url main_host_url, :last_mod => DateTime.now, :change_freq => 'daily', :priority => 1
+  url main_contact_url, :last_mod => DateTime.now, :change_freq => 'daily', :priority => 1
+    
 end
